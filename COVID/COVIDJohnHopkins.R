da40f3d086fa13d00b41a01af7578f9fd51a4bea
JoZelis <jo.zelis89@gmail.com>
2020-04-22 11:36:55 +0200

    COVID directly from JH

--- COVID/COVIDJohnHopkins.R
+++ COVID/COVIDJohnHopkins.R
@@ -7,30 +7,61 @@ library(tidyverse)
 library(lubridate)
 library(rvest)
 library(stringdist)
-
-covid_deaths <- read_csv(url("https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv"))
-
-y <- url("https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv")
-covid_confirmed <- read_csv(y)
-
+library(countrycode)
+#from Joachim Gassen See: https://github.com/joachim-gassen
 clean_jhd_to_long <- function(df) {
   df_str <- deparse(substitute(df))
   var_str <- substr(df_str, 1, str_length(df_str) - 4)
 
-  df %>% group_by(`Country/Region`) %>%
-    filter(`Country/Region` != "Cruise Ship") %>%
+  df %>%
     select(-`Province/State`, -Lat, -Long) %>%
-    mutate_at(vars(-group_cols()), sum) %>%
-    distinct() %>%
-    ungroup() %>%
     rename(country = `Country/Region`) %>%
+    mutate(iso3c = countrycode(country,
+                               origin = "country.name",
+                               destination = "iso3c")) %>%
+    select(-country) %>%
+    filter(!is.na(iso3c)) %>%
+    group_by(iso3c) %>%
+    summarise_at(vars(-group_cols()), sum) %>%
     pivot_longer(
-      -country,
+      -iso3c,
       names_to = "date_str",
       values_to = var_str
     ) %>%
+    ungroup() %>%
     mutate(date = mdy(date_str)) %>%
-    select(country, date, !! sym(var_str))
+    select(iso3c, date, !! sym(var_str))
 }
+
+confirmed_raw <- read_csv("https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv", col_types = cols())
+deaths_raw <- read_csv("https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_deaths_global.csv", col_types = cols())
+
+# Recovered data I pull from the old depreciated dataset. This might generate issues going forward
+recovered_raw <- read_csv("https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_recovered_global.csv", col_types = cols())
+
+jh_covid19_data <- clean_jhd_to_long(confirmed_raw) %>%
+  full_join(clean_jhd_to_long(deaths_raw), by = c("iso3c", "date")) %>%
+  full_join(clean_jhd_to_long(recovered_raw), by = c("iso3c", "date"))
+
+jhd_countries <- tibble(
+  country = unique(confirmed_raw$`Country/Region`),
+  iso3c = countrycode(country,
+                      origin = "country.name",
+                      destination = "iso3c")
+) %>% filter(!is.na(iso3c))
+
+old_jhd_countries <- tibble(
+  country = unique(recovered_raw$`Country/Region`),
+  iso3c = countrycode(country,
+                      origin = "country.name",
+                      destination = "iso3c")
+) %>% filter(!is.na(iso3c),
+             ! iso3c %in% jhd_countries$iso3c)
+
+jhd_countries <- rbind(jhd_countries, old_jhd_countries)
+
+jh_covid19_data %>%
+  left_join(jhd_countries, by = "iso3c") %>%
+  select(country, iso3c, date, confirmed, deaths, recovered) -> jh_covid19_data
 
 
